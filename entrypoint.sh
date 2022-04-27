0eff4feb1abb240f29198c74b68cbaa6b2e81eeb
iamtrazy <iamtrazy@protonmail.com>
2022-04-28 03:29:06 +0530

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#Config xray
+
+rm -rf /etc/xray/config.json
+cat << EOF > /etc/xray/config.json
+{
+  "inbounds": [
+    {
+      "port": $PORT,
+      "protocol": "trojan",
+      "settings": {
+        "clients": [
+          {
+            "password": "$PASSWORD"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}
+EOF
+
+#run xray
+
+xray -c /etc/xray/config.json
